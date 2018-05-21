68308100a2543072c67c160a938aba1705d25cbe
Grace Shih <vocalise@gmail.com>
2018-05-21 23:12:11 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -60,7 +60,6 @@ class MusicLibraryController
     songNumIndex = gets.strip.to_i
     reorderedSongs = Song.all.sort {|a,b| a.name <=> b.name}
     if songNumIndex-1 < reorderedSongs.length && songNumIndex >= 1 && songNumIndex <= 100
-      #binding.pry
       puts "Playing #{reorderedSongs[songNumIndex-1].name} by #{reorderedSongs[songNumIndex-1].artist.name}"
     end
   end
